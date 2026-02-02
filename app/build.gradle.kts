8f78cd27b1740e9550b34ee918015e87f9c26e50
Dagfinn Bakken <dagfinn@protonmail.com>
2026-02-03 07:38:16 +0800

    "Updated version number for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.ju.toob"
         minSdk = 29
         targetSdk = 35
-        versionCode = 24
-        versionName = "2.4"
+        versionCode = 26
+        versionName = "2.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
